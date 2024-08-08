aa8ca1d0dcb60238793c429d04b4849f6baf1b96
Vikash <yadav621gzp@gmail.com>
2024-08-08 06:49:23 +0530

    Footer name chnaged

--- Frontend/Component/v1.0.0/footer/footer.jsx
+++ Frontend/Component/v1.0.0/footer/footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
         <div className="flex mb-auto flex-wrap justify-center mx-auto">
          
           <div className="mt-2">
-            <h1 className="font-raleway text-3xl ml-3">OneTicket</h1>
+            <h1 className="font-raleway text-3xl ml-3">ReSell store</h1>
           
           </div>
         </div>
@@ -125,8 +125,8 @@ const Footer = () => {
         </div>
       </div>
       <div className="sm:px-40 flex justify-between text-sm py-10 bg-[#1B1B1B] text-white flex-col items-center sm:flex-row shadow-xl dark:bg-zinc-900">
-        <p>Copyright &copy; by OneTicket 2024</p>
-        <p className="mt-5 sm:mt-0">Powered by OneTicket</p>
+        <p>Copyright &copy; by ReSell 2024</p>
+        <p className="mt-5 sm:mt-0">Powered by ReSell</p>
       </div>
     </>
   );
