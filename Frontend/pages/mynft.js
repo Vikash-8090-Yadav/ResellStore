8008a8fe31c2d7c7a6399b9deff6a5a498f8c3a8
Vikash <yadav621gzp@gmail.com>
2024-08-08 06:47:08 +0530

    Name changed

--- Frontend/pages/mynft.js
+++ Frontend/pages/mynft.js
@@ -64,7 +64,7 @@ export default function MyAssets() {
   }
 
   if (loadingState === 'loaded' && !nfts.length)
-    return <h1 className="py-10 px-20  text-white text-3xl">No Ticket owned</h1>;
+    return <h1 className="py-10 px-20  text-white text-3xl">No Items owned</h1>;
 
   return (
     <div className="flex justify-center">
