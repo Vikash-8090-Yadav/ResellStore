06a8ac0ff170c8c92e9decc142d58c9b3b1fa776
Vikash <yadav621gzp@gmail.com>
2024-08-08 07:21:04 +0530

    Smart contract verified

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -1,6 +1,6 @@
 
 require ('@nomiclabs/hardhat-waffle');
-
+require('@nomiclabs/hardhat-etherscan');
 
 module.exports = {
   solidity: "0.8.10",
@@ -15,6 +15,22 @@ module.exports = {
       chainId: 11155111,
       accounts: ['5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702']
     },
-  }
+  },
+  etherscan: {
+    apiKey: "9VZPK5DTGX6V4Z81J8RY781S4U8CPQIANA",
+    customChains: [
+      {
+        network: "Sepolia",
+        chainId: parseInt("11155111"),
+        urls: {
+          apiURL: "https://eth-sepolia.blockscout.com/api",
+          browserURL: "https://eth-sepolia.blockscout.com/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false
+  } 
 };
 
